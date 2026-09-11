526f61911a371fbb68b6a4e9841eff13e48f9f75
Akshaykirdakar <75669007+Akshaykirdakar@users.noreply.github.com>
2026-09-11 13:05:36 +0530

    Add Guava library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,4 +87,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+implementation("com.google.guava:guava:33.2.1-android")
 }
